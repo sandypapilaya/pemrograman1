20805497720eb5e62a5fb447b6f6489aac235ec1
sandypapilaya <spapilaya@rocketmail.com>
2012-10-02 11:25:30 +0700

    sesi1

---
+++ pratikum/scr/sesi1/Person.java
@@ -0,0 +1,13 @@
+//Nama;Sandy
+//NIM;4511212013
+//Praktikum Pemrograman 4 - 1
+
+public class Person {
+public static void main(String[] args) {
+String nim = "4511212013";
+String nama;
+nama = "Sandy";
+System.out.println("Nama saya adalah:" +nama);
+System.out.println("Nim: " + nim);
+}
+}
